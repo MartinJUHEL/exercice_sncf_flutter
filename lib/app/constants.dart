baf4161079936121dd43c4381e619c2670b83872
MartinJUHEL <juhel.martin@gmail.com>
2022-09-13 21:10:31 +0200

    remove useless code

--- lib/app/constants.dart
+++
@@ -1,5 +0,0 @@
-class HeroTag {
-  static const logo = 'LOGO';
-  static const card_image = 'CARD_IMAGE';
-  static const recipe_image = 'RECIPE_IMAGE';
-}
--- lib/app/custom_styles.dart
+++ lib/app/custom_styles.dart
@@ -6,19 +6,6 @@ class CustomStyles {
     minimumSize: MaterialStateProperty.all(Size(double.infinity - 20,
         30)), // double.infinity is the width and 30 is the height
   );
-  static final deleteButton = ButtonStyle(
-    padding: MaterialStateProperty.all(EdgeInsets.fromLTRB(50, 20, 50, 20)),
-    minimumSize: MaterialStateProperty.all(Size(double.infinity - 20,
-        30)), // double.infinity is the width and 30 is the height
-  );
-
-  static final saveButton = ButtonStyle(
-    padding: MaterialStateProperty.all(EdgeInsets.fromLTRB(30, 10, 30, 10)),
-    shape: MaterialStateProperty.all(
-        RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0))),
-    minimumSize: MaterialStateProperty.all(
-        Size(250, 20)), // double.infinity is the width and 30 is the height
-  );
 
   static InputDecoration defaultTextFormFieldStyle(
       {String? labelTextStr = "",
